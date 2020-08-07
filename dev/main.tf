81564b56daf4a5abcd22138c53337c27a0c8f24b
LAPTOP-TOGP7PJ0\azigic <azigic@ztech.io>
2020-08-07 15:28:10 +0200

    corrected type

--- dev/main.tf
+++ dev/main.tf
@@ -3,7 +3,7 @@ terraform {
 }
 module "zf1" {
 //    source = "../module/ec2"
-    source = "git::https://github.com/lilixkube/zg-svc.git//dodule/ec2"
+    source = "git::https://github.com/lilixkube/zg-svc.git//module/ec2"
     instance_name = "zf-1"
     ami = "ami-06ce3edf0cff21f07"
     instance_type = "t2.micro"
